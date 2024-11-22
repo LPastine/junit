b4dbbae805ecdbae48ba86c48747efdd13d5146d
LPastine <lucianopastine.tech@gmail.com>
2024-11-22 18:01:05 -0300

    add same not same, true false, arrays, iterables and lines assertions

--- 1.00-starting-project/src/main/java/com/lpastine/junitdemo/DemoUtils.java
+++ 1.00-starting-project/src/main/java/com/lpastine/junitdemo/DemoUtils.java
@@ -1,6 +1,14 @@
 package com.lpastine.junitdemo;
 
+import java.util.List;
+
 public class DemoUtils {
+
+    private String academy = "Luv2Code Academy";
+    private String academyDuplicate = academy;
+
+    private String[] firstThreeLettersOfAlphabet = {"A", "B", "C"};
+    private List<String> academyInList = List.of("luv", "2", "code");
 
     public int add(int a, int b) {
         return a + b;
@@ -10,4 +18,23 @@ public class DemoUtils {
         return obj;
     }
 
+    public Boolean isGreater(int n1, int n2) {
+        return n1 > n2;
+    }
+
+    public String getAcademy() {
+        return academy;
+    }
+
+    public String getAcademyDuplicate() {
+        return academyDuplicate;
+    }
+
+    public String[] getFirstThreeLettersOfAlphabet() {
+        return firstThreeLettersOfAlphabet;
+    }
+
+    public List<String> getAcademyInList() {
+        return academyInList;
+    }
 }
--- 1.00-starting-project/src/test/java/com/lpastine/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/lpastine/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,8 @@ package com.lpastine.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
@@ -30,6 +32,47 @@ public class DemoUtilsTest {
 
         assertNull(demoUtils.checkNull(str1), "Object should be null");
         assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+    }
+
+    @Test
+    @DisplayName("Same and Not Same")
+    void testSameAndNotSame() {
+        String str = "luv2code";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer to same object");
+    }
+
+    @Test
+    @DisplayName("True and False")
+    void testTrueFalse() {
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @Test
+    @DisplayName("Array Equals")
+    void testArrayEquals() {
+        String[] stringArray = {"A", "B", "C"};
+
+        assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be the same");
+    }
+
+    @Test
+    @DisplayName("Iterable Equals")
+    void testIterableEquals() {
+        List<String> theList = List.of("luv", "2", "code");
+        assertIterableEquals(theList, demoUtils.getAcademyInList(), "Expected list should be the same as actual list");
+    }
+
+    @Test
+    @DisplayName("Lines Match")
+    void testLinesMatch() {
+        List<String> theList = List.of("luv", "2", "code");
+        assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
     }
 
 //    @AfterEach
